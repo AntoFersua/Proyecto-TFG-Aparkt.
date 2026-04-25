d50e2dc9911b2402f0ff240faaa5aaad8a4c9f47
Celia <cml0034@alu.medac.es>
2026-04-25 22:50:47 +0200

    pagina: Aparkt

---
+++ app/views/aparkt/estilosAparkt.js
@@ -0,0 +1,37 @@
+(() => {
+  const carrusel = $(".owl-carousel");
+
+  carrusel.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+
+    // Mejora UX
+    smartSpeed: 600,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: true,
+
+    // Interacción
+    mouseDrag: true,
+    touchDrag: true,
+
+    // Flechas personalizadas
+    navText: [
+      "<span class='arrow left'>‹</span>",
+      "<span class='arrow right'>›</span>"
+    ],
+
+    // Responsive por si luego quieres más slides
+    responsive: {
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    }
+  });
+})();
