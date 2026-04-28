debceeb2d531cf458068e20015dabc52e35e20b7
Celia <cml0034@alu.medac.es>
2026-04-28 22:39:14 +0200

    libreria Gsap

--- app/views/aparkt/estilosAparkt.js
+++ app/views/aparkt/estilosAparkt.js
@@ -21,17 +21,35 @@
     // Flechas personalizadas
     navText: [
       "<span class='arrow left'>‹</span>",
-      "<span class='arrow right'>›</span>"
+      "<span class='arrow right'>›</span>",
     ],
 
     // Responsive por si luego quieres más slides
     responsive: {
       768: {
-        items: 1
+        items: 1,
       },
       1024: {
-        items: 1
-      }
-    }
+        items: 1,
+      },
+    },
   });
 })();
+
+// Animaciones con GSAP
+gsap.registerPlugin(ScrollTrigger);
+const horizontalSection = document.querySelector(".horizontal");
+gsap.to(horizontalSection, {
+  x: () =>
+    -(horizontalSection.scrollWidth - document.documentElement.clientWidth) +
+    "px",
+  ease: "none",
+  scrollTrigger: {
+    trigger: horizontalSection,
+    start: "top top",
+    end: () => "+=" + (horizontalSection.scrollWidth - innerWidth),
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+  },
+});
